ba9b72afd8f477b8457ba2f57451be2c4cc67135
Chao‘s Assist <chaosassist@ChaosdeMac-mini.local>
2026-02-17 23:31:52 +0800

    Initial Celestial Tarot site - Astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://alexyuui.github.io',
   base: '/celestial-tarot-site',
+  integrations: [sitemap()],
 });
